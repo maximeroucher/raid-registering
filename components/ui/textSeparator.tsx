489576c160b12f498fa9d7248c107747765dca04
Maxime Roucher <maximeroucher@gmail.com>
2024-02-26 22:41:05 +0100

    feat: adding create account modal

---
+++ components/ui/textSeparator.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+
+const TextSeparator = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & {
+    text: string;
+  }
+>(({ text }) => (
+  <div className="relative">
+    <div className="absolute inset-0 flex items-center">
+      <span className="w-full border-t"></span>
+    </div>
+    <div className="relative flex justify-center text-xs uppercase">
+      <span className="bg-background px-2 text-muted-foreground">{text}</span>
+    </div>
+  </div>
+));
+
+TextSeparator.displayName = "TextSeparator";
+
+export { TextSeparator };
