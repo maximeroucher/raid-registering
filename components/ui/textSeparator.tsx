836db4aac69576133e057fa49b2775e8ea628efe
Maxime Roucher <maximeroucher@gmail.com>
2024-02-28 19:07:01 +0100

    fix: text separator

--- components/ui/textSeparator.tsx
+++ components/ui/textSeparator.tsx
@@ -2,19 +2,20 @@
 
 import * as React from "react";
 
-const TextSeparator = React.forwardRef<HTMLInputElement, { text: string }>(
-  ({ text }) => (
-    <div className="relative">
-      <div className="absolute inset-0 flex items-center">
-        <span className="w-full border-t"></span>
-      </div>
-      <div className="relative flex justify-center text-xs uppercase">
-        <span className="bg-background px-2 text-muted-foreground">{text}</span>
-      </div>
-    </div>
-  )
-);
+export type TextSeparatorProps = {
+  text: string;
+};
 
+const TextSeparator = ({ text }: TextSeparatorProps) => (
+  <div className="relative">
+    <div className="absolute inset-0 flex items-center">
+      <span className="w-full border-t"></span>
+    </div>
+    <div className="relative flex justify-center text-xs uppercase">
+      <span className="bg-background px-2 text-muted-foreground">{text}</span>
+    </div>
+  </div>
+);
 TextSeparator.displayName = "TextSeparator";
 
 export { TextSeparator };
