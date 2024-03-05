1ad1992c958d0e715dee6d0f5309c4d35ed5495d
Maxime Roucher <maximeroucher@gmail.com>
2024-03-05 19:04:09 +0100

    feat: adding create participant dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Participant, Team } from "../api/hyperionSchemas";
 import { EmptyParticipantCard } from "../components/home/EmptyParticipantCard";
 import { ParticipantCard } from "../components/home/ParicipantCard";
 import { TeamCard } from "../components/home/TeamCard";
@@ -8,18 +7,34 @@ import { TopBar } from "../components/home/TopBar";
 import { useAuth } from "../hooks/useAuth";
 import { useRouter } from "next/navigation";
 import { useTeam } from "../hooks/useTeam";
+import { useParticipant } from "../hooks/useParticipant";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../components/ui/dialog";
+import { Label } from "@radix-ui/react-label";
+import { Button } from "../components/ui/button";
+import { useState } from "react";
+import { Input } from "../components/ui/input";
 
 const Home = () => {
   const { isTokenQueried, token } = useAuth();
-  const { team, isLoading } = useTeam();
+  const { team } = useTeam();
+  const { me, isFetched } = useParticipant();
+
+  const [isOpened, setIsOpened] = useState(false);
 
   const router = useRouter();
 
   if (isTokenQueried && token === null) {
     router.replace("/login");
   }
+
+  
+  if (isFetched && me === undefined && !isOpened) {
+    console.log("me is undefined");
+    setIsOpened(true);
+  }
+
   return (
-    <>
+    <Dialog open={isOpened} onOpenChange={setIsOpened}>
       <TopBar />
       <main className="flex flex-col items-center">
         <div className="w-full px-24">
@@ -36,7 +51,40 @@ const Home = () => {
             </>
         </div>
       </main>
-    </>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
