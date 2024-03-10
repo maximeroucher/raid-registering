c1190e1d4e6b3f508dbc4e8cd33c1a9dc4b3b972
Maxime Roucher <maximeroucher@gmail.com>
2024-03-10 22:00:28 +0100

    feat: adding edit personal info

--- src/components/ui/datePicker.tsx
+++ src/components/ui/datePicker.tsx
@@ -23,7 +23,7 @@ export function DatePicker({ date, setDate, defaultDate }: DatePickerProps) {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[438px] justify-start text-left font-normal",
+            "w-full justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
         >
