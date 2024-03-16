4004c3bbab6b5c3ce4937e88ea493f44fb540a75
Maxime Roucher <maximeroucher@gmail.com>
2024-03-16 10:50:38 +0100

    chore: disabling error toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,16 +13,16 @@ import { toast } from "../components/ui/use-toast";
 const inter = Outfit({ subsets: ["latin-ext"] });
 
 const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error) => {
-      console.error(error);
-      toast({
-        title: "Erreur",
-        description: "Une erreur est survenue, veuillez réessayer plus tard",
-        variant: "destructive",
-      });
-    },
-  }),
+  // queryCache: new QueryCache({
+  //   onError: (error) => {
+  //     console.error(error);
+  //     toast({
+  //       title: "Erreur",
+  //       description: "Une erreur est survenue, veuillez réessayer plus tard",
+  //       variant: "destructive",
+  //     });
+  //   },
+  // }),
 });
 
 export default function RootLayout({
