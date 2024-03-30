faeadd4084080639e7d2d74a4eb7d486be41771e
Maxime Roucher <maximeroucher@gmail.com>
2024-03-30 13:39:16 +0100

    feat: adding loading

--- src/components/participantView/EmptyParticipantCard.tsx
+++ src/components/participantView/EmptyParticipantCard.tsx
@@ -5,21 +5,26 @@ import { toast } from "../ui/use-toast";
 import { Button } from "../ui/button";
 import { useInviteToken } from "@/src/hooks/useInviteToken";
 import { Team } from "@/src/api/hyperionSchemas";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
 interface EmptyParticipantCardProps {
   team?: Team;
 }
 
 export const EmptyParticipantCard = ({ team }: EmptyParticipantCardProps) => {
-  const { createInviteToken } = useInviteToken();
+  const { createInviteToken, isCreationLoading } = useInviteToken();
   function showToast() {
     if (team === undefined) {
       return;
     }
     createInviteToken(team.id, (token) => {
-      toast({
-        title: "Invitation envoyée",
-        description: `Le lien d'invitation est ${token.token}`,
+      const frontendUrl = process.env.NEXT_PUBLIC_FRONTEND_URL;
+      const inviteLink = `${frontendUrl}/invite/${token.token}`;
+      navigator.clipboard.writeText(inviteLink).then(() => {
+        toast({
+          title: "Invitation envoyée",
+          description: `Le lien d'invitation a été copié dans le presse-papier.`,
+        });
       });
     });
   }
@@ -27,9 +32,15 @@ export const EmptyParticipantCard = ({ team }: EmptyParticipantCardProps) => {
     <Card className="w-full" onClick={showToast}>
       <CardContent className="w-full h-full">
         <div className="flex m-auto h-full mt-3">
-          <Button variant="outline" className="flex m-auto">
-            <HiPlus className="inline mr-4" />
-            <span>Inviter un participant</span>
+          <Button variant="outline" className="flex m-auto w-[200px]" disabled={isCreationLoading}>
+            {isCreationLoading ? (
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              <>
+                <HiPlus className="inline mr-4" />
+                <span>Inviter un participant</span>
+              </>
+            )}
           </Button>
         </div>
       </CardContent>
