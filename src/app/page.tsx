25f237f7876bd6acff9f4c6692ceb9ee4bedb067
Maxime Roucher <maximeroucher@gmail.com>
2024-04-04 10:48:37 +0200

    feat: improving topbar and responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,11 +59,11 @@ const Home = () => {
         <JoinTeamDialog isOpened={isOpened} setIsOpened={setIsOpened} />
       )}
       <TopBar />
-      <main className="flex flex-col items-center">
-        <div className="w-full px-16">
+      <main className="flex flex-col items-center mt-4">
+        <div className="w-full px-14 max-md:px-8">
           <TeamCard team={team} />
         </div>
-        <div className="grid xl:grid-cols-2 gap-16 w-full p-16 grid-cols-1">
+        <div className="grid xl:grid-cols-2 gap-14 w-full p-14 grid-cols-1 max-md:p-8 max-md:gap-8">
           <ParticipantCard participant={team?.captain} isCaptain />
           {team?.second !== null ? (
             <ParticipantCard participant={team?.second} isCaptain={false} />
