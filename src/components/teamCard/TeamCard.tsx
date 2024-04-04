58971db37519cfa4d13a72d58578f2757308c82a
Maxime Roucher <maximeroucher@gmail.com>
2024-04-04 10:59:31 +0200

    fix: team card width for medium screen

--- src/components/teamCard/TeamCard.tsx
+++ src/components/teamCard/TeamCard.tsx
@@ -118,7 +118,7 @@ export const TeamCard = ({ team }: TeamCardProps) => {
           {isEdit && team ? (
             <TeamEdit team={team!} setIsEdit={setIsEdit} />
           ) : (
-            <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+            <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 xl:grid-cols-6">
               {information.map((info) => (
                 <TeamInfoCard
                   info={info}
