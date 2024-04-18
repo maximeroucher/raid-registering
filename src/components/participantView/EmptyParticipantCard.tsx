b426d52862f50f9ce1d5dd7ec2ae052ce3122e92
Maxime Roucher <maximeroucher@gmail.com>
2024-04-18 22:36:43 +0200

    fix: handling uncopiable toast text

--- src/components/participantView/EmptyParticipantCard.tsx
+++ src/components/participantView/EmptyParticipantCard.tsx
@@ -6,6 +6,7 @@ import { Button } from "../ui/button";
 import { useInviteToken } from "@/src/hooks/useInviteToken";
 import { Team } from "@/src/api/hyperionSchemas";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 interface EmptyParticipantCardProps {
   team?: Team;
@@ -13,45 +14,58 @@ interface EmptyParticipantCardProps {
 
 export const EmptyParticipantCard = ({ team }: EmptyParticipantCardProps) => {
   const { createInviteToken, isCreationLoading } = useInviteToken();
-  function showToast() {
+  const [invitationLink, setInvitationLink] = useState<string | null>(null);
+  function createInviteLink() {
     if (team === undefined) {
       return;
     }
     createInviteToken(team.id, async (token) => {
       const frontendUrl = process.env.NEXT_PUBLIC_FRONTEND_URL;
       const inviteLink = `${frontendUrl}?invite=${token.token}`;
+      setInvitationLink(inviteLink);
       try {
         await navigator.clipboard.writeText(inviteLink);
         toast({
-          title: "Invitation créée",
+          title: "Invitation envoyée",
           description: `Le lien d'invitation a été copié dans le presse-papier.`,
         });
-      } catch (e) {
+      } catch (error) {
         toast({
-          title: "Invitation créée",
-          description: `Le lien d'invitation est : ${inviteLink}`,
+          variant: "destructive",
+          title: "Erreur",
+          description: `Impossible de copier le lien d'invitation dans le presse-papier.`,
         });
       }
     });
   }
   return (
-    <Card className="w-full" onClick={showToast}>
+    <Card className="w-full">
       <CardContent className="w-full h-full">
-        <div className="flex m-auto h-full mt-3">
-          <Button
-            variant="outline"
-            className="flex m-auto w-[200px]"
-            disabled={isCreationLoading}
-          >
-            {isCreationLoading ? (
-              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-            ) : (
-              <>
-                <HiPlus className="inline mr-4" />
-                <span>Inviter un participant</span>
-              </>
+        <div className="flex m-auto h-full mt-3 flex-col">
+          <div className="flex m-auto flex-col items-center">
+          
+            
+            <Button
+              variant="outline"
+              className="w-[200px]"
+              disabled={isCreationLoading}
+              onClick={createInviteLink}
+            >
+              {isCreationLoading ? (
+                <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <>
+                  <HiPlus className="inline mr-4" />
+                  <span>Inviter un participant</span>
+                </>
+              )}
+            </Button>
+            {invitationLink && (
+              <span className="text-sm text-muted-foreground mt-4">
+                {invitationLink}
+              </span>
             )}
-          </Button>
+            </div>
         </div>
       </CardContent>
     </Card>
