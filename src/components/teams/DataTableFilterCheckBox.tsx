accaa210b6c6510f9aae3f3f7f900c94cab6129b
Maxime Roucher <maximeroucher@gmail.com>
2024-04-30 14:59:24 +0200

    fix: data table button style

--- src/components/teams/DataTableFilterCheckBox.tsx
+++ src/components/teams/DataTableFilterCheckBox.tsx
@@ -1,21 +1,6 @@
 import * as React from "react";
-import { CheckIcon, PlusCircledIcon } from "@radix-ui/react-icons";
 import { Column } from "@tanstack/react-table";
 
-import { cn } from "@/lib/utils";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { Button } from "../ui/button";
-import { Separator } from "../ui/separator";
-import { Badge } from "../ui/badge";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from "../ui/command";
 import { Checkbox } from "../ui/checkbox";
 
 interface DataTableFilterCheckBoxProps<TData, TValue> {
@@ -27,14 +12,13 @@ export function DataTableFilterCheckBox<TData, TValue>({
   column,
   title,
 }: DataTableFilterCheckBoxProps<TData, TValue>) {
-  const facets = column?.getFacetedUniqueValues();
   const selectedValues = new Set(column?.getFilterValue() as null[]);
 
   const filterValues = Array.from(selectedValues);
   const isSelected = filterValues.length > 0;
 
   return (
-    <div className="h-8 border-dashed border rounded-md items-center flex px-3">
+    <div className="h-8 border-dashed border rounded-md items-center flex px-3 justify-center whitespace-nowrap text-sm font-medium ">
       <Checkbox
         checked={isSelected}
         className="mr-2"
