0f1ab3642a2848a3ea01913e921f705c1cef2d3e
Maxime Roucher <maximeroucher@gmail.com>
2024-05-01 15:56:22 +0200

    fix: error on logout

--- src/components/userSheet/logoutButton.tsx
+++ src/components/userSheet/logoutButton.tsx
@@ -1,24 +1,14 @@
 import { useAuth } from "@/src/hooks/useAuth";
 import { HiLogout } from "react-icons/hi";
 import { Button } from "../ui/button";
-import { useUserStore } from "@/src/stores/user";
-import { useParticipantStore } from "@/src/stores/particpant";
 
 export const LogoutButton = () => {
   const { logout } = useAuth();
-  const { setUser } = useUserStore();
-  const { setParticipant } = useParticipantStore();
-
-  function handleLogout() {
-    setUser(undefined);
-    setParticipant(undefined);
-    logout();
-  }
 
   return (
     <Button
       variant="ghost"
-      onClick={handleLogout}
+      onClick={logout}
       className="px-2 py-1.5 text-sm font-medium w-full justify-start hover:bg-background text-destructive"
     >
       Se déconnecter
