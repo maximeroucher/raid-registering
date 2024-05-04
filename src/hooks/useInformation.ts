31e195f16816897365dd6ccab1c4d604f84cea43
Maxime Roucher <maximeroucher@gmail.com>
2024-05-04 12:02:44 +0200

    feat: adding information hook

---
+++ src/hooks/useInformation.ts
@@ -0,0 +1,61 @@
+import {
+  useGetRaidInformation,
+  usePatchRaidInformation,
+} from "@/src/api/hyperionComponents";
+import { RaidInformation } from "../api/hyperionSchemas";
+import { useAuth } from "./useAuth";
+
+export const useInformation = () => {
+  const { token, userId } = useAuth();
+
+  const {
+    data: information,
+    isLoading,
+    refetch: refetchInformation,
+  } = useGetRaidInformation(
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+    {
+      enabled: userId !== null,
+      retry: 0,
+      queryHash: "getRaidInformation",
+    },
+  );
+
+  const { mutate: mutateUpdateInformation, isPending: isUpdateLoading } =
+    usePatchRaidInformation({});
+
+  const updateInformation = (
+    callback: () => void,
+    information?: RaidInformation,
+  ) => {
+    mutateUpdateInformation(
+      {
+        body: information,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      {
+        // Not using onSucess because of : https://github.com/TanStack/query/discussions/2878
+        onSettled: () => {
+          // Assuming success in all cases
+          // For unknown reasons, the invalidation of the query does not work
+          refetchInformation();
+          callback();
+        },
+      },
+    );
+  };
+
+  return {
+    information,
+    isLoading,
+    refetchInformation,
+    updateInformation,
+    isUpdateLoading,
+  };
+};
