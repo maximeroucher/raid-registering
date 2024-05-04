be281e412cc4fe6f76c3b1d829754ec365a21bc2
Maxime Roucher <maximeroucher@gmail.com>
2024-05-04 12:16:38 +0200

    feat: adding dates to team card

--- src/components/teamCard/TeamCard.tsx
+++ src/components/teamCard/TeamCard.tsx
@@ -5,8 +5,6 @@ import {
   Card,
   CardHeader,
   CardTitle,
-  CardDescription,
-  CardContent,
 } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 import { Button } from "../ui/button";
@@ -25,6 +23,9 @@ import {
   meetingPlaces,
 } from "@/src/infra/comboboxValues";
 import { TeamInfoCard } from "./TeamInfoCard";
+import { useInformation } from "@/src/hooks/useInformation";
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 
 interface TeamCardProps {
   team?: Team;
@@ -32,12 +33,35 @@ interface TeamCardProps {
 
 export const TeamCard = ({ team }: TeamCardProps) => {
   const [isEdit, setIsEdit] = useState(false);
+  const { information } = useInformation();
 
   function toggleEdit() {
     setIsEdit(!isEdit);
   }
 
-  const information = [
+  function getDaysLeft(dateString: string) {
+    const date = new Date(dateString);
+    const now = new Date();
+    return Math.floor((date.getTime() - now.getTime()) / (1000 * 60 * 60 * 24));
+  }
+
+  function formatDateRange(start: string, end: string) {
+    const startDateDay = format(new Date(start), "dd", { locale: fr });
+    const startDateMonth = format(new Date(start), "MMMM", { locale: fr });
+    const endDateDay = format(new Date(end), "dd", { locale: fr });
+    const endDateMonth = format(new Date(end), "MMMM", { locale: fr });
+    const startDateYear = format(new Date(start), "yyyy", { locale: fr });
+    const endDateYear = format(new Date(end), "yyyy", { locale: fr });
+    if (startDateYear === endDateYear) {
+      if (startDateMonth === endDateMonth) {
+        return `${startDateDay} - ${endDateDay} ${startDateMonth} ${startDateYear}`;
+      } else {
+        return `${startDateDay} ${startDateMonth} - ${endDateDay} ${endDateMonth} ${startDateYear}`;
+      }
+    }
+  }
+
+  const informationCard = [
     {
       title: "Lieu de rendez-vous",
       value: getLabelFromValue(meetingPlaces, team?.meeting_place ?? undefined),
@@ -45,15 +69,22 @@ export const TeamCard = ({ team }: TeamCardProps) => {
       unit: <HiOutlineLocationMarker className="h-4 w-4" />,
     },
     {
-      title: "Date",
-      value: "06-07 octobre",
+      title: "Dates",
+      value:
+        information?.raid_start_date && information?.raid_end_date
+          ? formatDateRange(information.raid_start_date, information.raid_end_date)
+          : "Dates non renseignées",
       description: "week-end complet",
       unit: <HiOutlineCalendar className="h-4 w-4" />,
     },
     {
       title: "Inscription",
       value: `${team?.validation_progress.toFixed(0)}%`,
-      description: "10 jours restants",
+      description: information?.raid_registering_end_date
+        ? `${getDaysLeft(
+            information?.raid_registering_end_date,
+          )} jours restants`
+        : "Date de fin non renseignée",
       unit: <span>%</span>,
     },
     {
@@ -110,7 +141,7 @@ export const TeamCard = ({ team }: TeamCardProps) => {
             <TeamEdit team={team!} setIsEdit={setIsEdit} />
           ) : (
             <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-              {information.map((info) => (
+              {informationCard.map((info) => (
                 <TeamInfoCard
                   info={info}
                   key={info.title}
