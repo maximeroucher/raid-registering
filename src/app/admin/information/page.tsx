1e224f094c037ca5339fa01bd9d5ab902ae4bb91
Maxime Roucher <maximeroucher@gmail.com>
2024-06-03 22:43:19 +0200

    feat: adding file edition buttons

--- src/app/admin/information/page.tsx
+++ src/app/admin/information/page.tsx
@@ -6,7 +6,9 @@ import { DriveFolder } from "@/src/components/admin/information/DriveFolder";
 import { EmergencyPerson } from "@/src/components/admin/information/EmergencyPersons";
 import { InscriptionEnd } from "@/src/components/admin/information/InscriptionEnd";
 import { RaidDate } from "@/src/components/admin/information/RaidDate";
+import { RaidInformation } from "@/src/components/admin/information/RaidInformation";
 import { RaidPrice } from "@/src/components/admin/information/RaidPrice";
+import { RaidRules } from "@/src/components/admin/information/RaidRules";
 import { TShirtPrice } from "@/src/components/admin/information/TShirtPrice";
 import {
   Card,
@@ -44,6 +46,8 @@ const InformationPage = () => {
               <RaidPrice />
               <TShirtPrice />
               <EmergencyPerson />
+              <RaidRules />
+              <RaidInformation />
             </div>
           </CardContent>
         </Card>
