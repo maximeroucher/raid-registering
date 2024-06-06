5274ad230324b61699c4bc89188a3f712d5d8179
Maxime Roucher <maximeroucher@gmail.com>
2024-06-06 17:45:26 +0200

    wip: required node js

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,21 +13,58 @@ import {
 } from "@/src/components/ui/card";
 import MyECLButton from "../../components/login/MyECLButton";
 import Link from "next/link";
+import { Issuer } from "openid-client";
+import { generators } from "openid-client";
+import { useRouter } from "next/navigation";
+import { useCodeVerifierStore } from "@/src/stores/codeVerifier";
+import { LoadingButton } from "@/src/components/custom/LoadingButton";
 
-const Login = () => {
+function Login() {
+  const router = useRouter();
+  const { setCodeVerifier } = useCodeVerifierStore();
+  
+  async function openSSO() {
+    console.log("dscover", process.env.NEXT_PUBLIC_BACKEND_URL ?? "");
+    const hyperionIssuer = await Issuer.discover(
+        "https://hyperion-3.dev.eclair.ec-lyon.fr",
+    );
+    console.log('ezgrshgj')
+
+    const client = new hyperionIssuer.Client({
+      client_id: process.env.NEXT_PUBLIC_CLIENT_ID ?? "",
+      redirect_uris: [process.env.NEXT_PUBLIC_FRONTEND_URL ?? ""],
+      response_types: ["code"],
+    });
+
+    const codeVerifier = generators.codeVerifier();
+    setCodeVerifier(codeVerifier);
+    const codeChallenge = generators.codeChallenge(codeVerifier);
+
+    const url = client.authorizationUrl({
+      scope: "API",
+      code_challenge: codeChallenge,
+      code_challenge_method: "SHA256",
+    });
+    router.push(url);
+  }
   return (
     <div className="flex [&>div]:w-full h-screen">
       <Card className="rounded-xl border bg-card text-card-foreground shadow max-w-[700px] m-auto text-zinc-700">
         <CardHeader>
           <CardTitle>Se connecter</CardTitle>
           <CardDescription>
-            Si vous possédez déjà un compte MyECL, vous pouvez vous connecter avec.
+            Si vous possédez déjà un compte MyECL, vous pouvez vous connecter
+            avec.
           </CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <div className="grid w-full items-center gap-4">
-              <MyECLButton />
+              <LoadingButton
+                isLoading={false}
+                onClick={openSSO}
+                label="Se connecter"
+              />
             </div>
           </form>
         </CardContent>
--- src/components/login/MyECLButton.tsx
+++ src/components/login/MyECLButton.tsx
@@ -4,31 +4,28 @@ import { useAuth } from "../../hooks/useAuth";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCodeVerifierStore } from "@/src/stores/codeVerifier";
 import { useEffect, useState } from "react";
-import { LoadingButton } from "../custom/LoadingButton";
 
-const Login = () => {
+async function Login() {
   const { token, isTokenExpired, login, isLoading, getTokenFromRequest } =
     useAuth();
-  const router = useRouter();
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
-  const { codeVerifier } = useCodeVerifierStore();
   const [isLoggingIn, setIsLoggingIn] = useState(false);
 
-  useEffect(() => {
-    if (
-      code &&
-      typeof window !== "undefined" &&
-      !isLoading &&
-      codeVerifier !== undefined &&
-      !isLoggingIn
-    ) {
-      setIsLoggingIn(true);
-      login(code, () => {
-        router.replace("/");
-      });
-    }
-  }, [code, isLoading, codeVerifier, login, router, isLoggingIn]);
+  // useEffect(() => {
+  //   if (
+  //     code &&
+  //     typeof window !== "undefined" &&
+  //     !isLoading &&
+  //     codeVerifier !== undefined &&
+  //     !isLoggingIn
+  //   ) {
+  //     setIsLoggingIn(true);
+  //     login(code, () => {
+  //       router.replace("/");
+  //     });
+  //   }
+  // }, [code, isLoading, codeVerifier, login, router, isLoggingIn]);
 
   if (token !== null && !isTokenExpired()) {
     router.replace("/");
@@ -39,13 +36,10 @@ const Login = () => {
     getTokenFromRequest();
   }
 
+  
+
   return (
-    <LoadingButton
-      isLoading={isLoading}
-      onClick={connectMyECL}
-      label="Se connecter"
-    />
   );
-};
+}
 
 export default Login;
